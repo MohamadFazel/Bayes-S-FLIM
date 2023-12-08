90d42b0c412fa337c930d4f9ce339d97b6f37768
reza <qrezahosseiny@gmail.com>
2023-12-08 22:35:23 +0330

    label

--- modules/data_loader.py
+++ modules/data_loader.py
@@ -4,5 +4,6 @@ import numpy as np
 def load_data_and_lambda(file_path):
     data = sio.loadmat(file_path)
     dt_mix = np.squeeze(data["Dt"]).reshape(128, 128)[64:, :64].reshape(-1)
-    lam_mix = data["Lambda"].reshape(-1, 128, data["Lambda"].shape[1])[64:, :64].reshape(-1, data["Lambda"].shape[2])
+    lam_mix = data["Lambda"].reshape(-1, 128, data["Lambda"].shape[1])[64:, :64]
+    lam_mix = lam_mix.reshape(-1, lam_mix.shape[2])
     return dt_mix, lam_mix
