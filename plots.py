eed968676e3ecf0b7160fe0c9ad3a41cd6b128a2
Reza <qrezahosseiny@gmail.com>
2025-03-04 08:56:31 +0330

    asix

--- plots.py
+++ plots.py
@@ -6,7 +6,7 @@ import numpy as np
 # put location of data here
 eta_dir = ""
 pi_dir = ""
-phi_dir = "/media/reza/44ec9f87-1051-4bdf-8f53-fcf9d10c68a5/SpectralFlim/NewResult/lmv_Phot_1218155633.npy"
+phi_dir = ""
 
 save_path = "results"
 
@@ -25,7 +25,7 @@ pin = np.mean(pi[-num:, :, :], axis=0)
 
 plt.figure(figsize=(16, 16))
 for it in range(pin.shape[0]):
-    plt.plot(pin[it] / np.sum(pin[it]), plot_color[it], label=f"spectra {it}")
+    plt.plot(pin[it] / np.sum(pin[it]), plot_color[it], label=f"spectra {it+1}")
 
 plt.legend()
 plt.title("Species Spectra")
@@ -38,7 +38,7 @@ plt.show()
 
 plt.figure(figsize=(16, 16))
 for eta_it in range(eta.shape[1]):
-    plt.hist(1 / eta[-num:, eta_it], bins=100, color=plot_color[eta_it], label=f"eta {eta_it}")
+    plt.hist(1 / eta[-num:, eta_it], bins=100, color=plot_color[eta_it], label=f"eta {eta_it+1}")
 plt.legend()
 plt.title("Lifetimes Histogram")
 plt.xlabel("Lifetime (ns)")
@@ -53,6 +53,6 @@ for phi_it in range(phi.shape[0]):
     plt.figure(figsize=(16, 16))
     plt.imshow(phi[phi_it], cmap=imshow_color[phi_it])
     plt.colorbar()
-    plt.title(f"map {phi_it}")
-    plt.savefig(f"{save_path}/map_{phi_it}.png")
+    plt.title(f"map {phi_it+1}")
+    plt.savefig(f"{save_path}/map_{phi_it+1}.png")
     plt.show()
